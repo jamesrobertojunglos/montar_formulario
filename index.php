4fc0976916530164d05762eb49d8480381e4c2b6
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-09-01 22:57:38 -0300

    Códigos para montar um formulário.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>  
+
+<hr>
+<?php
+    $email = '';
+    $nome = '';
+    if (isset($_POST['enviar'])) {
+        if (empty($_POST['email'])) {
+            echo "informe o e-mail <br>";
+        } else {
+            $email = $_POST['email'];
+        }
+        if (empty($_POST['nome'])) {
+            echo "informe o nome <br>";
+        } else {
+            $nome = $_POST['nome'];
+        }
+      
+    }
+   
+?>
+<form method="POST">
+    <input type="text" name="email" value="<?php echo $email ?>">
+    <input type="text" name="nome" value="<?php echo $nome ?>">
+    <input type="submit" value="enviar" name="enviar">
+</form>
+
+   
+</body>
+</html>
